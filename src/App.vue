ebd47db25823a06df750bb578d5d7103eb963a74
Kacper Pawlak <58702798+Pabl01802@users.noreply.github.com>
2024-03-06 21:32:40 +0100

    made navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useHead } from '@vueuse/head'
+import NavBar from './components/NavBar.vue'
 
-export default {
-  name: 'App',
+export default ({
+  setup(){
+    useHead({
+      meta: [
+        {
+          name: 'description',
+          content: 'Kormix - firma zajmująca się wywozem nieczystości płynnych'
+        },
+        {
+          name: 'keywords',
+          content: 'Kormix, kormix, wywóz, wywóz nieczystości, wywóz nieczystości płynnych'
+        }
+      ]
+    })
+  },
+  name: 'KORMIX - zapewniamy konkurencyjne ceny',
   components: {
-    HelloWorld
+    NavBar
   }
-}
+})
 </script>
 
 <style>
+body{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,88 @@
+<template>
+  <nav>
+    <div class="nav-top">
+      <div class="hamburger">
+        <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg>
+      </div>
+      <ul class="navigation">
+        <li>
+          <a href="/">Strona główna</a>
+        </li>
+        <li>
+          <a href="about">O nas</a>
+        </li>
+        <li>
+          <a href="contact">Kontakt</a>
+        </li>
+      </ul>
+      <img class="logo" alt="Kormix logo" src='../assets/logoDark.png'>
+    </div>
+    <h4 class="nav-header">wywóz nieczystości płynnych</h4>
+  </nav>
+</template>
+
+<script>
+  export default{
+    name: 'NavBar'
+  }
+</script>
+
+<style scoped>
+  .hamburger{
+    display: none;
+  }
+  nav{
+    padding: 0;
+    margin: 0;
+    position: relative;
+  }
+  .nav-top{
+    height: 80px;
+    position: relative;
+    display: flex;
+  }
+  .navigation{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    width: 300px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+  li{
+    padding: 15px;
+    display: grid;
+    place-items: center;
+  }
+  .logo{
+    width: 150px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .nav-header{
+    text-align: center;
+    margin: 0;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+
+  @media(max-width: 768px){
+    .navigation{
+      display: none;
+    }
+    .nav-top{
+      align-items: center;
+      padding-left: 20px;
+    }
+    .hamburger{
+      display: flex;
+      height: max-content;
+    }
+    svg{
+      width: 40px;
+    }
+  }
+</style>
